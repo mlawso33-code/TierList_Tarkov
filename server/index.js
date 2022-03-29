69ad1d6ca83c71065df1868809a7649f0a0d1159
mlawso33-code <marclawson195@gmail.com>
2022-03-28 23:52:47 -0700

    fixed commas

--- server/index.js
+++ server/index.js
@@ -1,44 +1,44 @@
-const express = require('express')
-const axios = require('axios')
-const db = require('../db/index.js')
+const express = require('express');
+const axios = require('axios');
+const db = require('../db/index.js');
 const shrinkRay = require('shrink-ray-current');
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/', express.static(__dirname + '/../client/dist'))
-app.use(shrinkRay())
+app.use('/', express.static(__dirname + '/../client/dist'));
+app.use(shrinkRay());
 
 //retrieve weapons
 app.get('/tierlist/assault_rifles/:id', (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   db.query(`SELECT * FROM assault_rifles WHERE id=${id}`, (err, result) => {
-    err ? console.log(err) : res.send(result)
-  })
-})
+    err ? console.log(err) : res.send(result);
+  });
+});
 app.get('/tierlist/posts', (req, res) => {
-  db.query(`SELECT * FROM forumPosts`, (err, result) => {
-    err ? console.log(err) : res.send(result)
-  })
-})
+  db.query('SELECT * FROM forumPosts', (err, result) => {
+    err ? console.log(err) : res.send(result);
+  });
+});
 app.get('/tierlist/posts/:id/replies', (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   db.query(`SELECT * FROM forumReplies WHERE post_id=${id}`, (err, result) => {
-    err ? console.log(err) : res.send(result)
-  })
-})
+    err ? console.log(err) : res.send(result);
+  });
+});
 
 //update votes
 app.put('/tierlist/assault_rifles/:id', (req, res) => {
-  const { id } = req.params
-  const { vote } = req.body
+  const { id } = req.params;
+  const { vote } = req.body;
   db.query(`UPDATE assault_rifles SET votes=${vote} WHERE id=${id}`, (err, result) => {
-    err ? console.log(err) : res.sendStatus(201)
-  })
-})
+    err ? console.log(err) : res.sendStatus(201);
+  });
+});
 
 
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, (err) => {
-  err ? console.log(err) : console.log(`Listening on ${PORT}`)
-})
+  err ? console.log(err) : console.log(`Listening on ${PORT}`);
+});
