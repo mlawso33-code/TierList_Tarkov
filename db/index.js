a32f0d8a3428c9cf012c3c07a710f1017b22fad2
mlawso33-code <marcravens330@gmail.com>
2022-10-02 17:55:46 -0400

    fixed sql connection

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 // get the client
 const mysql = require('mysql2');
+require('dotenv').config({path: `../.env`});
 
 // create the connection to database
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'mlawso33',
-  password: 'password',
-  database: 'tierList',
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
   port: 3306,
 });
 
